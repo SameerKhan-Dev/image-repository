6c52cf3d5e0c1e5730ccfdba1256ececa94ad7c7
Sameer Khan <sameerk_786@hotmail.com>
2021-01-16 00:55:22 +0000

    setup new pool connection for psql

--- database/database.js
+++ database/database.js
@@ -1,28 +1,24 @@
 require('dotenv').config();
 
-
 // const { rows } = require('pg/lib/defaults');
 
 const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || "development";
 
-
+// setup pool client and connect
 const { Pool } = require('pg');
+// const dbParams = require('../lib/db.js');
 
 const pool = new Pool( {
-  user: 'hello',
-  password: '123',
-  host:'localhost',
-  database:'lightbnb'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+pool.connect((err,client, release) => {
+    console.log("Pool Connected!") 
 });
 
-const { Pool } = require('pg');
-const dbParams = require('../lib/db.js');
-
-// console.log(dbParams);
-
-const pool = new Pool(dbParams);
-pool.connect();
-
-
+// 
 module.exports = pool;
