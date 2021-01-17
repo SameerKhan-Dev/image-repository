8ef3116376a01c2c2fa279258da43024a3f195ca
Sameer Khan <sameerk_786@hotmail.com>
2021-01-17 04:10:34 +0000

    created ejs files for adding images and main page, and user registration page etc.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 // Web server config
 const database = require('./database/database');
-
+const fileUpload = require('express-fileupload');
 //const path = require('path');
 
 const PORT = process.env.PORT || 8080;
@@ -21,7 +21,8 @@ const databaseHelper = require('./database/databaseHelpers/databaseHelpers');
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 //app.use(morgan('dev'));
-
+// use m 
+app.use(fileUpload());
 app.use(cookieSession({
   name: 'session',
   keys: ['b6d0e7eb-8c4b-4ae4-8460-fd3a08733dcb', '1fb2d767-ffbf-41a6-98dd-86ac2da9392e']
@@ -43,6 +44,11 @@ app.use(express.static("public"));
 const userRouter = require('./routes/users');
 const imageRouter = require('./routes/images');
 
+app.get("/main", (req, res) => {
+
+  res.render('main_page');
+
+});
 
 app.use("/users", userRouter);
 app.use("/images", imageRouter);
