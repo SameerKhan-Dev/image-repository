d39d1349602542dad38ed94e4a94270feba3016a
Sameer Khan <sameerk_786@hotmail.com>
2021-01-18 04:53:27 +0000

    add creds.js with actual credentials cleaned out

---
+++ creds.js
@@ -0,0 +1,10 @@
+module.exports = {
+    host: 'localhost',
+    port: 5432,
+    user: 'vagrant',
+    database: 'imagerep',
+    password: 'insert-your-db-pw',
+    s3Bucket: 'insert-your-bucket-name-here',
+    accessKey: 'insert-your-aws-acces-key-here',
+    secretAccessKey: 'insert-your-aws-secret-access-key-here'
+};
